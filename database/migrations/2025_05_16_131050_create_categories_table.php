d2ccc7df81ad5858e154cb388301214568565508
morteza <mortezaghube@gmail.com>
2025-05-20 01:29:45 +0300

    frontend view implimented

--- database/migrations/2025_05_16_131050_create_categories_table.php
+++ database/migrations/2025_05_16_131050_create_categories_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
-            $table->char('sluge',255);
+            $table->char('slug',255);
             $table->char('title',255);
             $table->timestamps();
         });
