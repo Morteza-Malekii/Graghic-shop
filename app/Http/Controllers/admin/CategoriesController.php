00db59e04e4eb8a9ee112d56ad354fed79bbdb5d
morteza <mortezaghube@gmail.com>
2025-05-28 23:26:22 +0300

    storing categories implemented

---
+++ app/Http/Controllers/admin/CategoriesController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers\admin;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Admin\Categories\StoreCategory;
+use App\Http\Requests\Admin\Categories\StoreRequest;
+use App\Models\Category;
+use Illuminate\Auth\Events\Validated;
+use Illuminate\Http\Request;
+
+class CategoriesController extends Controller
+{
+    public function create(){
+        return view('admin.categories.create');
+    }
+
+    public function store(StoreRequest $request)
+    {
+        // $storeRequest = $request->validate();
+        $categoryStore = Category::create([
+            'title'=>$request->title,
+            'slug'=>$request->slug
+        ]);
+        if (!$categoryStore)
+            return back()->with('failed','category is not created !');
+        return back()->with('success','category is create succcessfully!');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\admin\CategoriesController;
+use GuzzleHttp\Promise\Create;
 use Illuminate\Support\Facades\Route;
 
 Route::get('products/all', function(){
@@ -8,4 +10,11 @@ Route::get('products/all', function(){
 
 Route::get('products/admin',function(){
     return view('admin.index');
+});
+
+Route::prefix('admin')->group(function(){
+    Route::prefix('categories')->group(function(){
+        Route::get('create',[CategoriesController::class , 'Create']);
+        Route::post('store' , [CategoriesController::class , 'store'])->name('admin.categories.store');
+    });
 });
